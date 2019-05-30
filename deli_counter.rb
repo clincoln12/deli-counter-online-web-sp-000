91da48c64ff1bba4bd11d63535ce6cb89c0ca4cc
Chris Lincoln <chris.lincoln12@gmail.com>
2019-05-30 17:45:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     arr = []
-    arr << "The line is currently: "
+    arr << "The line is currently:"
     katz_deli.each_with_index { |x, i| arr << "#{i + 1}. #{x}" }
     puts arr.join(" ")
   end
