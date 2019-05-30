523718d4c45ade6863452bbe7bdf679bf96478d2
Chris Lincoln <chris.lincoln12@gmail.com>
2019-05-30 20:15:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,5 +19,5 @@ end
 def now_serving(katz_deli)
   puts "There is nobody waiting to be served!" if katz_deli.empty?
   
-  katz_deli - [0] unless katz_deli.empty?
+  katz_deli -= [0] unless katz_deli.empty?
 end
