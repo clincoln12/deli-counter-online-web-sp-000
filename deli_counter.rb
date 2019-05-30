ab14034dc9c983ed9819e73820a09764c9b27e9d
Chris Lincoln <chris.lincoln12@gmail.com>
2019-05-30 16:44:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@
 def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
-  elsif
+  else
     katz_deli.each_with_index { |x, i| puts "The line is currently: #{i + 1}. #{x}" }
   end
 end
