bb3b22b0b992264ca602c902a7c77bc1dd052ff6
Chris Lincoln <chris.lincoln12@gmail.com>
2019-05-30 20:12:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,8 @@ def take_a_number(katz_deli, name)
     katz_deli << name
     puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
 end
+
+def now_serving(katz_deli)
+  puts "Empty string" if katz_deli.empty?
+  
+end
